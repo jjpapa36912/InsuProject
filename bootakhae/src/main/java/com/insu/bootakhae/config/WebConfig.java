ca93fa62818bb46db1ebe2f2f4e2b4838abd7f70
김동준 <djk@gimdongjun-ui-MacBookPro.local>
2023-11-29 23:06:52 +0900

    로그인 체크 기능 / Login Argument Resolver 추가

---
+++ bootakhae/src/main/java/com/insu/bootakhae/config/WebConfig.java
@@ -0,0 +1,40 @@
+package com.insu.bootakhae.config;
+
+import com.insu.bootakhae.web.argumentresolver.LoginMemberArgumentResolver;
+import com.insu.bootakhae.web.interceptor.LogInterceptor;
+import com.insu.bootakhae.web.interceptor.LoginCheckInterceptor;
+import java.util.List;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+    resolvers.add(new LoginMemberArgumentResolver());
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new LogInterceptor())
+        .order(1)
+        .addPathPatterns("/**")
+        .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+    registry.addInterceptor(new LoginCheckInterceptor())
+        .order(2)
+        .addPathPatterns("/**")
+        .excludePathPatterns("/", "/members/add", "/login", "/logout", "/css/**", "/*.ico", "/error");
+  }
+
+  @Bean
+  public ModelMapper modelMapper() {
+    return new ModelMapper();
+  }
+
+}
